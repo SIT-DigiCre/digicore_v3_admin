583f8a0a559da2f18843fcda0ca358a67c500f4c
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2023-02-09 22:54:58 +0900

    DBへの接続を修正

--- util/db.ts
+++ util/db.ts
@@ -2,10 +2,11 @@ import mysql from "mysql2";
 import { resolve } from "path";
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "digicre",
-  database: "digicre",
+  host:
+    process.env.NODE_ENV === "development" ? "localhost" : process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
 connection.connect((err) => {
   if (err) {
