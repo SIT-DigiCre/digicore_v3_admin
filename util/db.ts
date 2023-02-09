79ead6a2c4e42e2a03a1acd93acd5deef9ab1281
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2023-02-09 23:05:23 +0900

    HOSTの修正

--- util/db.ts
+++ util/db.ts
@@ -2,8 +2,7 @@ import mysql from "mysql2";
 import { resolve } from "path";
 
 const connection = mysql.createConnection({
-  host:
-    process.env.NODE_ENV === "development" ? "localhost" : process.env.DB_HOST,
+  host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
