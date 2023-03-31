170b4a7f321869e35430e266bdcd1e45c3d0b7a5
ymd <74826010+ymd1138@users.noreply.github.com>
2023-03-31 20:17:39 +0900

    フィルタの項目を追加、画面左右に余白を追加

--- pages/users.tsx
+++ pages/users.tsx
@@ -14,29 +14,49 @@ const UsersPage = ({ users }: Props) => {
   const [selectTypes, setSelectTypes] = useState<TableDataType[]>([]);
   const onCheckFilter = (checked: boolean, type: TableDataType) => {
     if (checked) setSelectTypes([...selectTypes, type]);
-    else setSelectTypes(selectTypes.filter(t => t !== type));
-  }
+    else setSelectTypes(selectTypes.filter((t) => t !== type));
+  };
   return (
-    <div style={{ width: "100%" }} className="overflow-auto">
-      {allTableDataTypes.map(t => (
+    <div style={{ width: "90vw", margin: "auto" }} className="overflow-auto">
+      {allTableDataTypes.map((t) => (
         <>
-          <input type="checkbox" key={t} onChange={(e) => { onCheckFilter(e.target.checked, t) }} />
+          <input
+            type="checkbox"
+            key={t}
+            onChange={(e) => {
+              onCheckFilter(e.target.checked, t);
+            }}
+          />
           {t}
         </>
       ))}
-      <Table striped bordered hover style={{ tableLayout: "fixed" }}>
+      <Table
+        striped
+        bordered
+        hover
+        style={{ tableLayout: "fixed", overflow: "scroll" }}
+      >
         <thead>
           <tr>
-            {
-              // 幅は調整中。細かい調整は後ほど。
-            }
-            {selectTypes.indexOf("id") !== -1 ? <th style={{ width: 150 }}>ID</th> : <></>}
-
+            {/* 幅は調整中。細かい調整は後ほど。*/}
+            {selectTypes.indexOf("id") !== -1 ? (
+              <th style={{ width: 150 }}>ID</th>
+            ) : (
+              <></>
+            )}
             <th className="table-th-studentNumber">学籍番号</th>
             <th className="table-th-username">ユーザー名</th>
             <th style={{ width: 30 }}>学年</th>
-            <th style={{ width: 66 }}>アイコン</th>
-            <th style={{ width: 100 }}>Discord ID</th>
+            {selectTypes.indexOf("icon") !== -1 ? (
+              <th style={{ width: 66 }}>アイコン</th>
+            ) : (
+              <></>
+            )}
+            {selectTypes.indexOf("discord") !== -1 ? (
+              <th style={{ width: 100 }}>Discord ID</th>
+            ) : (
+              <></>
+            )}
             <th style={{ width: 100 }}>有効期限</th>
             <th style={{ width: 150 }}>自己紹介</th>
             <th className="table-th-name">名字</th>
@@ -56,14 +76,21 @@ const UsersPage = ({ users }: Props) => {
           {users.map((user) => (
             <tr key={user.id}>
               {selectTypes.indexOf("id") !== -1 ? <td>{user.id}</td> : <></>}
-
               <td>{user.studentNumber}</td>
               <td>{user.username}</td>
               <td>{user.schoolGrade}</td>
-              <td>
-                <img src={user.iconUrl} alt="" style={{ width: "50px" }} />
-              </td>
-              <td>{user.discordUserId}</td>
+              {selectTypes.indexOf("icon") !== -1 ? (
+                <td>
+                  <img src={user.iconUrl} alt="" style={{ width: "50px" }} />
+                </td>
+              ) : (
+                <></>
+              )}
+              {selectTypes.indexOf("discord") !== -1 ? (
+                <td>{user.discordUserId}</td>
+              ) : (
+                <></>
+              )}
               <td>{user.activeLimit}</td>
               <td>{user.shortIntroduction}</td>
               <td>{user.firstName}</td>
