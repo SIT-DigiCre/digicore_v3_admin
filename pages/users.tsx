afa9c63b468a65d53fa892114fad13ff48fdec6a
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2023-05-01 17:42:11 +0900

    エラー対応処理を追加

--- pages/users.tsx
+++ pages/users.tsx
@@ -6,6 +6,7 @@ import { User } from "./api/user";
 
 type Props = {
   users: User[];
+  error?: string;
 };
 type TableDataType = "公開情報" | "有効期限・ID" | "個人情報" | "保護者情報";
 
@@ -34,7 +35,7 @@ const addressStyle = {
   width: "200px",
 };
 
-const UsersPage = ({ users }: Props) => {
+const UsersPage = ({ users, error }: Props) => {
   const [selectTypes, setSelectTypes] = useState<TableDataType[]>([
     ...defaultTableDataTypes,
   ]);
@@ -44,6 +45,13 @@ const UsersPage = ({ users }: Props) => {
   };
   return (
     <Container fluid>
+      {error ? (
+        <Row>
+          <p style={{ color: "red" }}>{error}</p>
+        </Row>
+      ) : (
+        <></>
+      )}
       <Row>
         <div className="p-2">
           {allTableDataTypes.map((t) => (
@@ -181,67 +189,73 @@ const UsersPage = ({ users }: Props) => {
 export default UsersPage;
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const rows = await queryDb(
-    `SELECT 
-    BIN_TO_UUID(users.id) as id, 
-    student_number, 
-    username, 
-    school_grade, 
-    icon_url, 
-    discord_userid, 
-    active_limit, 
-    short_introduction,
-    first_name,
-    last_name,
-    first_name_kana,
-    last_name_kana,
-    is_male,
-    phone_number,
-    address,
-    parent_name,
-    parent_cellphone_number,
-    parent_homephone_number,
-    parent_address
-  FROM 
-    users 
-  LEFT JOIN 
-    user_profiles 
-  ON 
-    users.id=user_profiles.user_id 
-  LEFT JOIN 
-    user_private_profiles 
-  ON 
-    users.id=user_private_profiles.user_id`
-  );
-  const users: User[] = rows.map((r) => {
-    return {
-      id: r.id,
-      studentNumber: r.student_number,
-      username: r.username,
-      schoolGrade: r.school_grade,
-      iconUrl: r.icon_url,
-      discordUserId: r.discord_userid,
-      activeLimit: r.active_limit.toLocaleString("ja-JP"),
-      shortIntroduction: r.short_introduction,
-      firstName: r.first_name,
-      lastName: r.last_name,
-      firstNameKana: r.first_name_kana,
-      lastNameKana: r.last_name_kana,
-      isMale: r.is_male == 1 ? true : false,
-      phoneNumber: r.phone_number,
-      address: r.address,
-      parentName: r.parent_name,
-      parentCellphoneNumber: r.parent_cellphone_number,
-      parentHomephoneNumber: r.parent_homephone_number,
-      parentAddress: r.parent_address,
+  try {
+    const rows = await queryDb(
+      `SELECT 
+      BIN_TO_UUID(users.id) as id, 
+      student_number, 
+      username, 
+      school_grade, 
+      icon_url, 
+      discord_userid, 
+      active_limit, 
+      short_introduction,
+      first_name,
+      last_name,
+      first_name_kana,
+      last_name_kana,
+      is_male,
+      phone_number,
+      address,
+      parent_name,
+      parent_cellphone_number,
+      parent_homephone_number,
+      parent_address
+    FROM 
+      users 
+    LEFT JOIN 
+      user_profiles 
+    ON 
+      users.id=user_profiles.user_id 
+    LEFT JOIN 
+      user_private_profiles 
+    ON 
+      users.id=user_private_profiles.user_id`
+    );
+    const users: User[] = rows.map((r) => {
+      return {
+        id: r.id,
+        studentNumber: r.student_number,
+        username: r.username,
+        schoolGrade: r.school_grade,
+        iconUrl: r.icon_url,
+        discordUserId: r.discord_userid,
+        activeLimit: r.active_limit.toLocaleString("ja-JP"),
+        shortIntroduction: r.short_introduction,
+        firstName: r.first_name,
+        lastName: r.last_name,
+        firstNameKana: r.first_name_kana,
+        lastNameKana: r.last_name_kana,
+        isMale: r.is_male == 1 ? true : false,
+        phoneNumber: r.phone_number,
+        address: r.address,
+        parentName: r.parent_name,
+        parentCellphoneNumber: r.parent_cellphone_number,
+        parentHomephoneNumber: r.parent_homephone_number,
+        parentAddress: r.parent_address,
+      };
+    });
+
+    const props: Props = {
+      users,
     };
-  });
 
-  const props: Props = {
-    users,
-  };
-
-  return {
-    props: props,
-  };
+    return {
+      props: props,
+    };
+  } catch (e: any) {
+    return {
+      props: { users: [], error: e.message },
+    };
+  }
 };
